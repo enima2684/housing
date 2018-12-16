1ad96fee672a7c8fcafe85e35fab5523742091d3
enima2684 <amine.bouamama@gmail.com>
2018-12-16 21:36:18 +0100

    modify criterias

--- housing/Scrapper/SeLoger.py
+++ housing/Scrapper/SeLoger.py
@@ -5,7 +5,7 @@ class SeLoger(Scrapper):
 
     def __init__(self):
         super(SeLoger, self).__init__()
-        self.url = "https://www.seloger.com/list.htm?types=1,2&projects=2&enterprise=0&natures=1,2,4&price=NaN/450000&surface=45/NaN&bedrooms=2&places=[{cp:75}|{ci:920050}|{ci:920026}|{ci:920062}|{ci:920063}|{ci:780146}]&qsVersion=1.0&abSLC=new"
+        self.url = "https://www.seloger.com/list.htm?types=1,2&projects=2&enterprise=0&natures=1,2,4&price=NaN/550000&surface=45/NaN&bedrooms=2&places=[{cp:75}|{ci:920050}|{ci:920026}|{ci:920062}|{ci:920063}|{ci:780146}]&qsVersion=1.0&abSLC=new"
         self.web_site = "SeLoger"
 
 
